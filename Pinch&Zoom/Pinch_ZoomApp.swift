7aaa60e876f78e3870e09e3960a45288879d82ed
willianbogarin <willianbogarin@hotmail.com>
2023-09-20 12:04:01 +0200

    Initial Commit

---
+++ Pinch&Zoom/Pinch_ZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_ZoomApp.swift
+//  Pinch&Zoom
+//
+//  Created by Willian Bogarin Jr on 2023. 09. 20..
+//
+
+import SwiftUI
+
+@main
+struct Pinch_ZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
