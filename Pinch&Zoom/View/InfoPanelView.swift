59961c216d4357c18e07166880cf85f0218b0ac8
willianbogarin <willianbogarin@hotmail.com>
2023-09-23 13:35:32 +0200

    Implemented, Drag, ontap and longtap Gesture to the app

---
+++ Pinch&Zoom/View/InfoPanelView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoPanelView.swift
+//  Pinch&Zoom
+//
+//  Created by Willian Bogarin Jr on 2023. 09. 23..
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoPanelView()
+}
